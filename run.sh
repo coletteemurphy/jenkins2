98143516c2454bed5a260eff3ac65bbfa29cecdc
colette <colette@test.com>
2024-06-12 16:29:16 +0100

    push again with fixed secret

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 echo "Hello AGAIN from the Jenkins job named: ${JOB_NAME}"
 echo "Do it again"
 echo "this change shouldn't do a push on jenkins"
+echo "this should now"
 touch 1.txt 2.txt 3.txt 4.txt 5.txt
 zip archive.zip *.txt
