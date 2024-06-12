fcd3f88a97bf7b7231be7ce98f347b806b4bf804
colette <colette@test.com>
2024-06-12 16:27:58 +0100

    push but secret wrong

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 echo "Hello AGAIN from the Jenkins job named: ${JOB_NAME}"
 echo "Do it again"
+echo "this change shouldn't do a push on jenkins"
 touch 1.txt 2.txt 3.txt 4.txt 5.txt
 zip archive.zip *.txt
